83688151ce6c64a8b923af3a452cb5eb79717cc9
10Cdegree <armin.szathmari@outlook.com>
2022-03-17 11:12:16 +0100

    added fetch

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -1,12 +1,34 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import LoadingMask from "./components/LoadingMask";
+import Laptops from "./components/Laptops";
+
+const url = "https://demoapi.com/api/laptop";
+
+const fetchLaptops = async () => {
+  const res = await fetch(url);
+  return res.json();
+};
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchLaptops().then((chars) => {
+      setCharacters(chars);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <div>
-      Todo...
+      {loading ? (
+        <LoadingMask />
+      ) : (
+        characters.map((char) => <Laptops key={char.name} {...char} />)
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
